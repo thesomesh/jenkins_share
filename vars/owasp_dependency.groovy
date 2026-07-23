8d67104395f4578c45fb1843907b5cf592a1bc58
thesomesh <91958243+thesomesh@users.noreply.github.com>
2026-07-23 09:56:53 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -3,7 +3,7 @@ def call() {
     dependencyCheck(
         odcInstallation: 'OWASP',
         nvdCredentialsId: 'nvd-api-key',
-        additionalArguments:  '--scan . \'
+        additionalArguments: '--scan ./' 
     )
 
     dependencyCheckPublisher(
