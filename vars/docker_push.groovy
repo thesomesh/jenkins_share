e7c100f09a28865a78736d7a15efcb36424465ac
thesomesh <91958243+thesomesh@users.noreply.github.com>
2026-08-30 15:53:03 +0530

    Refactor docker_push for improved security and clarity
    Refactor docker_push function to use password-stdin for login and improve variable naming.

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,23 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-  }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+
+def call(String Project, String ImageTag) {
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'docker',
+            usernameVariable: 'DH_USER',
+            passwordVariable: 'DH_PASS'
+        )
+    ]) {
+
+        sh '''
+            echo "$DH_PASS" | docker login \
+                --username "$DH_USER" \
+                --password-stdin
+        '''
+
+        sh "docker push ${DH_USER}/${Project}:${ImageTag}"
+
+        sh 'docker logout'
+    }
 }
+
